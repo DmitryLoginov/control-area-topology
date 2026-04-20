5beaa640961ce4e14e32ae9c99251e6e9ddd8000
DmitryLoginov <daimom25@gmail.com>
2026-04-20 22:02:23 +0300

    feat: add model classes

---
+++ ControlAreaTopologyProcessor/Model/ControlArea.cs
@@ -0,0 +1,41 @@
+﻿namespace ControlAreaTopologyProcessor.Model
+{
+    public class ControlArea
+    {
+        public Guid Uid { get; } = Guid.NewGuid();
+        public string name { get; set; }
+        public TieFlow[] TieFlows { get; private set; } = [];
+
+        public void AddToTieFlows(TieFlow tieFlow)
+        {
+            if (!TieFlows.Contains(tieFlow))
+            {
+                var temp = TieFlows;
+                Array.Resize<TieFlow>(ref temp, TieFlows.Length + 1);
+                temp[^1] = tieFlow;
+                TieFlows = temp;
+            }
+        }
+
+        public void RemoveFromTieFlows(TieFlow tieFlow)
+        {
+            if (TieFlows.Contains(tieFlow))
+            {
+                var temp = TieFlows.ToList<TieFlow>();
+                temp.Remove(tieFlow);
+                TieFlows = temp.ToArray();
+            }
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is ControlArea area &&
+                   Uid.Equals(area.Uid);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Uid);
+        }
+    }
+}
---
+++ ControlAreaTopologyProcessor/Model/TieFlow.cs
@@ -0,0 +1,41 @@
+﻿namespace ControlAreaTopologyProcessor.Model
+{
+    public class TieFlow
+    {
+        public Guid Uid { get; } = Guid.NewGuid();
+        public string name { get; set; }
+        public bool positiveFlowIn { get; set; }
+        public Terminal Terminal { get; set; }
+        public ControlArea ControlArea { get; set; }
+
+        /// <summary>
+        /// Creates a tie flow for given control area and equipment terminal.
+        /// </summary>
+        /// <param name="terminal">Conducting equipment terminal.</param>
+        /// <param name="controlArea">Control area.</param>
+        /// <param name="positiveFlowIn">Does equipment belong to control area?</param>
+        /// <remarks>
+        /// Control area and terminal got their corresponding links updated with this tie flow.
+        /// </remarks>
+        public TieFlow(Terminal terminal, ControlArea controlArea, bool positiveFlowIn)
+        {
+            this.Terminal = terminal;
+            this.ControlArea = controlArea;
+            this.positiveFlowIn = positiveFlowIn;
+
+            terminal.AddToTieFlows(this);
+            controlArea.AddToTieFlows(this);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is TieFlow flow &&
+                   Uid.Equals(flow.Uid);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Uid);
+        }
+    }
+}
