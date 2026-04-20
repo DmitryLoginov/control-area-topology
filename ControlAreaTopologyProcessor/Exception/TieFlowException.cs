d752c867330a5a2691d687a765c0e48ba79ac5b6
DmitryLoginov <daimom25@gmail.com>
2026-04-20 22:03:39 +0300

    feat: add TieFlowException

---
+++ ControlAreaTopologyProcessor/Exception/TieFlowException.cs
@@ -0,0 +1,7 @@
+﻿namespace ControlAreaTopology.Exception
+{
+    public class TieFlowException : ApplicationException
+    {
+        public TieFlowException(String message) : base(message) { }
+    }
+}
